47d7687979298cf16c0d979f503b65d1f4989433
Adithep <badsxx@gmail.com>
2014-07-23 18:35:01 +0700

    my eyes

---
+++ img_to_tags.js
@@ -0,0 +1,94 @@
+(function () {
+  "use strict";
+
+  var walk = require('walk')
+    , fs = require('fs')
+    , options
+    , walker
+    , arr = []
+    ;
+  var obj = {
+    "Aetas Residence": "ELwKPCaJARdyYFkzq",
+    "Sarawan Condominium": "FuvWq2sGea3bxSeQM",
+    "Seacon Square": "D6D4RiZQZ7nQFqS27",
+    "The Promenade": "t6WFM8CCgGiD3TC5L",
+    "Mccann": "FGADmqtBKdT5Amxyk",
+    "Right Man": "jucfEpoga6MgdW2vT",
+    "Laad Prao Office": "jdi37ZrXd6z9FDziX",
+    "Zen": "EmCxofXsebsXQHJLY",
+    "Air Bar": "vFD4jn97ez4Em7Tr4",
+    "Moon Bar": "5QSJaYoCNCarWrc7y",
+    "The Roof": "KMSLHowfDHZqNtGe7",
+    "Ananta": "NpTRr8hX5ueask3mW",
+    "The Scene": "oWZuGMmvMQttrMT2a"
+  };
+
+  var obj2 = {
+    "Aetas Residence": "Condo",
+    "Sarawan Condominium": "Condo",
+    "Seacon Square": "Mall",
+    "The Promenade": "Mall",
+    "Mccann": "Office",
+    "Right Man": "Office",
+    "Laad Prao Office": "Office",
+    "Zen": "Restaruant",
+    "Air Bar": "Restaruant",
+    "Moon Bar": "Restaruant",
+    "The Roof": "Restaruant",
+    "Ananta": "Restaruant",
+    "The Scene": "Mall"
+  };
+
+  // To be truly synchronous in the emitter and maintain a compatible api,
+  // the listeners must be listed before the object is created
+  options = {
+    listeners: {
+      names: function (root, nodeNamesArray) {
+      }
+    , directories: function (root, dirStatsArray, next) {
+        // dirStatsArray is an array of `stat` objects with the additional attributes
+        // * type
+        // * error
+        // * name
+
+        next();
+      }
+    , file: function (root, fileStats, next) {
+        var rr = root.replace("./tem/", "");
+
+        if (obj[rr]) {
+          var lo1 = {
+            key_1_key: "location_id",
+            key_1_s: "locations",
+            _v_1: obj[rr],
+            key_2_key: "img_uuid",
+            key_2_s: "img",
+            _v_2: fileStats.name
+          };
+
+          var lo2 = {
+            key_key: "location_ty_n",
+            key_s: "location_ty",
+            _v: obj2[rr],
+            key_2_key: "img_uuid",
+            key_2_s: "img",
+            _v_2: fileStats.name
+          };
+          arr.push(lo1);
+          arr.push(lo2);
+        } else {
+          console.log("cant " + rr)
+        }
+        
+      }
+    , errors: function (root, nodeStatsArray, next) {
+        next();
+      }
+    }
+  };
+
+  walker = walk.walkSync("./tem", options);
+  var str = JSON.stringify(arr);
+  fs.writeFileSync('tags.json', str);
+  console.log("done");
+}());
