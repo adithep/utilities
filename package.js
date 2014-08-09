76f89ed1a92036ce762734373426a4c45af1dd34
Adithep <badsxx@gmail.com>
2014-08-09 12:46:02 +0700

    ma

--- package.js
+++ package.js
@@ -1,14 +1,19 @@
 Package.describe({
-  summary: "provides utilities"
+  summary: "provides utilities",
+  version: "0.0.1"
 });
 
 Package.on_use(function (api, where) {
-  api.use(['core-lib', 'coffeescript']);
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
+  api.use([
+    'bads:core-lib',
+    'coffeescript'
+  ]);
   api.add_files(['phoneformat.js', 'emailformat.coffee', 'fuzzy-search.js'], ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use('utilities');
+  api.use("../packages/bads:utilities");
 
   api.add_files('utilities_tests.js', ['client', 'server']);
 });
